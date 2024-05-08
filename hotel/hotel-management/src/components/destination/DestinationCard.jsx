177fa3058e9370d2f3fc5872c67ec4248883f6b1
HanYoonAh <geethukrishna3718@gmail.com>
2024-05-09 00:45:17 +0530

    Rating has been added

--- hotel/hotel-management/src/components/destination/DestinationCard.jsx
+++ hotel/hotel-management/src/components/destination/DestinationCard.jsx
@@ -1,14 +1,20 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import ReactStars from "react-stars";
 
-function DestinationCard({name,image,alt}) {
+function DestinationCard({ name, image, alt }) {
   return (
-    <Card style={{ width: "18rem"  }}>
-      <Card.Img variant="top" src={image} alt={alt} width={286} height={189}/>
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image} alt={alt} width={286} height={189} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
         <Card.Text>
-         Rating:
+          <p>
+            Rating:
+            <span>
+              <ReactStars count={5} size={24} color2={"#ffd700"} />
+            </span>
+          </p>
         </Card.Text>
         <Button variant="primary">Show Hotels</Button>
       </Card.Body>
